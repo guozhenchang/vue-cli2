d18b37818c2db8521b3835ea114ce1cc3f4e2bbd
guozhenchang <guozhenchang@qq.com>
2019-07-09 11:21:00 +0800

    0.0.1
    基于vue-cli2的个人用vue项目，配置了常用的功能。

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello" v-title="msg">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted() {
+    this.$store.state.abb = '12';
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+
+</style>
